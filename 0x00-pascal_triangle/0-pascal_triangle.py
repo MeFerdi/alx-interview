d16e317978967bf77f6ff5254fea091055bd486b
MeFerdi <ferdinandodhis254@gmail.com>
2024-10-07 10:43:55 +0300

    Chore: implement function to calculate pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize with the first row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        # Generate the next row by adding the adjacent values of the previous row
+        row = [1]  # Start with 1
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)  # End with 1
+        triangle.append(row)
+
+    return triangle
