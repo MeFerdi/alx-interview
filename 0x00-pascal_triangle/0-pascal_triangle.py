ff91f4ee2238a6695c473122b6dc6adac7b9bdb7
MeFerdi <ferdinandodhis254@gmail.com>
2024-10-07 10:46:56 +0300

    Chore: implement function to calculate pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """A script to determine pascal's triangle for any number"""
 
+
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    triangle = []
+
+    # return (trianlgle if n <= 0)
     if n <= 0:
-        return []
+        return triangle
+    for i in range(n):
+        temp_list = []
 
-    triangle = [[1]]  # Initialize with the first row
-
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        # Generate the next row by adding the adjacent values of the previous row
-        row = [1]  # Start with 1
-        for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
-        row.append(1)  # End with 1
-        triangle.append(row)
-
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp_list)
+    # print(triangle)
     return triangle
