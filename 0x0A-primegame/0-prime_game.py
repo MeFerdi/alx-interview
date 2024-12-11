ab2d8456873b77e2e58798493f78873fdae3bbfe
MeFerdi <ferdinandodhis254@gmail.com>
2024-12-12 00:52:59 +0300

    Chore: format to PEP8 rule

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,73 +1,29 @@
 #!/usr/bin/python3
+""" Module for solving prime game question """
 
 def isWinner(x, nums):
-    """
-    Determines the winner of a prime number game played over x rounds.
-
-    Parameters:
-    x (int): The number of rounds to be played.
-    nums (list): A list of integers representing the upper limit (n) of the set 
-                 of consecutive integers for each round.
-
-    Returns:
-    str: The name of the player who won the most rounds ("Maria" or "Ben").
-         Returns None if there is no winner.
-    """
-    def sieve_of_eratosthenes(n):
-        """
-        Generates all prime numbers up to n using the Sieve of Eratosthenes.
-
-        Parameters:
-        n (int): The upper limit for generating prime numbers.
-
-        Returns:
-        list: A list of all prime numbers less than or equal to n.
-        """
-        primes = [True] * (n + 1)
-        primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-        return [i for i in range(2, n + 1) if primes[i]]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    # Simulate each round
-    for n in nums:
-        primes = sieve_of_eratosthenes(n)
-        remaining = [True] * (n + 1)  # Track numbers still available in the game
-        turn = 0  # 0 for Maria's turn, 1 for Ben's turn
-
-        while True:
-            # Find the first available prime
-            prime_to_pick = None
-            for prime in primes:
-                if prime <= n and remaining[prime]:
-                    prime_to_pick = prime
-                    break
-
-            if prime_to_pick is None:
-                # No primes available, current player loses
-                if turn == 0:
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            # Mark the chosen prime and its multiples as removed
-            for i in range(prime_to_pick, n + 1, prime_to_pick):
-                remaining[i] = False
-
-            # Switch turn (0 -> Maria, 1 -> Ben)
-            turn = 1 - turn
-
-    # Determine the final winner based on who won the most rounds
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
-    
+    max_num = max(nums)
+
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
